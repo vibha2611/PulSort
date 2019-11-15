174d4011ebb1dfe756b058d8a5146fb82f799b3c
Vibha Padmanabhan <57306517+vibha2611@users.noreply.github.com>
2019-11-15 14:25:19 +0530

    Added pixel combo for GBNCC plots

--- phase-subband-crop.py
+++ phase-subband-crop.py
@@ -1,6 +1,11 @@
 from PIL import Image
 import os.path, sys
 
+x=int(input("Enter 1 if GBT plot, 2 if GBNCC plot: "))
+if x==1:
+        y=(295, 150, 505, 405)
+else:
+        y=(700, 420, 1110, 950)
 path = r"folder" #enter name of folder containing plots
 dirs = os.listdir(path)
 for item in dirs:
@@ -8,5 +13,5 @@ for item in dirs:
         if os.path.isfile(fullpath):
             im = Image.open(fullpath)
             f, e = os.path.splitext(fullpath)
-            imCrop = im.crop((295, 150, 505, 405)) 
+            imCrop = im.crop(y) 
             imCrop.save(f + ' phase-subband.png', "png", quality=100)
